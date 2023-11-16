3fe48d4c17fe1bbcc4e5ff0f4e4184e5cd35fd5a
robert-wienroeder <136348469+robert-wienroeder@users.noreply.github.com>
2023-11-16 22:31:36 +0100

    Update and rename SELECT_HOURS to temporal_development.sql

--- SELECT/temporal_development.sql
+++ SELECT/temporal_development.sql
@@ -1,9 +1,10 @@
 SELECT DISTINCT
     p.QUERY,
+    t.WEEKDAY,
+    t."day of the year",
     t."hour",
     f.ANONID,
     f.TIMEID
 FROM FACTS f
 JOIN PORN_QUERIES p ON f.QUERYID = p.ID
-JOIN TIMEDIM t ON f.TIMEID = t.ID
-ORDER BY t."hour" ASC;
+JOIN TIMEDIM t ON f.TIMEID = t.ID;
