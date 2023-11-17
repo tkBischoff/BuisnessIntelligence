f9a632d131b937bd747c931234034a51f5e4d17a
robert-wienroeder <136348469+robert-wienroeder@users.noreply.github.com>
2023-11-17 14:31:04 +0100

    Update temporal_development.sql

--- SELECT/temporal_development.sql
+++ SELECT/temporal_development.sql
@@ -1,10 +1,13 @@
-SELECT DISTINCT
+SELECT
     p.QUERY,
     t.WEEKDAY,
     t."day of the year",
     t."hour",
     f.ANONID,
-    f.TIMEID
-FROM FACTS f
-JOIN PORN_QUERIES p ON f.QUERYID = p.ID
-JOIN TIMEDIM t ON f.TIMEID = t.ID;
+    f.TIMEID,
+    COUNT(*)
+FROM EXA_DB.FACTS f
+JOIN EXA_DB.PORN_QUERIES p ON f.QUERYID = p.ID
+JOIN EXA_DB.TIMEDIM t ON f.TIMEID = t.ID
+GROUP BY ROLLUP(p.QUERY, t.WEEKDAY, t."day of the year", t."hour", f.ANONID, f.TIMEID)
+HAVING COUNT(*) > 1;
