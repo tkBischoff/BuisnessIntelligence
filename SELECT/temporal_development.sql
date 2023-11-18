e8907b2fc316d347716a45dd45f31a21a26eac8c
robert-wienroeder <136348469+robert-wienroeder@users.noreply.github.com>
2023-11-18 11:59:05 +0100

    Update temporal_development.sql

--- SELECT/temporal_development.sql
+++ SELECT/temporal_development.sql
@@ -5,9 +5,19 @@ SELECT
     t."hour",
     f.ANONID,
     f.TIMEID,
-    COUNT(*)
+    COUNT(*) AS record_count
 FROM EXA_DB.FACTS f
 JOIN EXA_DB.PORN_QUERIES p ON f.QUERYID = p.ID
 JOIN EXA_DB.TIMEDIM t ON f.TIMEID = t.ID
-GROUP BY ROLLUP(p.QUERY, t.WEEKDAY, t."day of the year", t."hour", f.ANONID, f.TIMEID)
-HAVING COUNT(*) > 1;
+GROUP BY GROUPING SETS (
+    (p.QUERY, f.ANONID, f.TIMEID, t."day of the year"),
+    (p.QUERY, f.ANONID, f.TIMEID, t.WEEKDAY),
+    (p.QUERY, f.ANONID, f.TIMEID, t."hour")
+)
+ORDER BY
+    CASE
+        WHEN GROUPING(t."day of the year") = 0 THEN t."day of the year"
+        WHEN GROUPING(t.WEEKDAY) = 0 THEN t.WEEKDAY
+        WHEN GROUPING(t."hour") = 0 THEN t."hour"
+    END,
+    p.QUERY, f.ANONID, f.TIMEID;
