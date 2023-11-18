fc2e4d310f13736045b81d85c03fdd2f12ac9a53
MaxwellMuF <148557718+MaxwellMuF@users.noreply.github.com>
2023-11-19 00:06:58 +0100

    Update URL_ended_session.sql

--- ETL/URL_ended_session.sql
+++ ETL/URL_ended_session.sql
@@ -6,7 +6,9 @@ WITH SessionData AS (
         "hour", "minute", "second",
         "URL",
         CASE
-            WHEN (LEAD("TIME_SPAMP_SECOND") OVER (PARTITION BY AnonID ORDER BY AnonID,"day of the year", "hour", "minute", "second") - "TIME_SPAMP_SECOND") > 60*60*0.5 AND "TIME_SPAMP_SECOND" != LEAD("TIME_SPAMP_SECOND") OVER (PARTITION BY AnonID ORDER BY AnonID, "day of the year", "hour", "minute", "second") OR ANONID != LEAD(ANONID) OVER (PARTITION BY AnonID ORDER BY AnonID, "day of the year", "hour", "minute", "second") THEN 'session_end'
+            WHEN (LEAD("TIME_SPAMP_SECOND") OVER (PARTITION BY AnonID ORDER BY AnonID,"day of the year", "hour", "minute", "second") - "TIME_SPAMP_SECOND") > 60*60*0.5 AND 
+                "TIME_SPAMP_SECOND" != LEAD("TIME_SPAMP_SECOND") OVER (PARTITION BY AnonID ORDER BY AnonID, "day of the year", "hour", "minute", "second") OR 
+                ANONID != LEAD(ANONID) OVER (PARTITION BY AnonID ORDER BY AnonID, "day of the year", "hour", "minute", "second") THEN 'session_end'
             ELSE 'TRUE'
         END AS is_session
     FROM
