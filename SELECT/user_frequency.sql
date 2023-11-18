332c1f0545fd5f1e0918a01a2bf7c5dca0173257
robert-wienroeder <136348469+robert-wienroeder@users.noreply.github.com>
2023-11-18 12:19:15 +0100

    Update user_frequency.sql

--- SELECT/user_frequency.sql
+++ SELECT/user_frequency.sql
@@ -1,16 +1,15 @@
-SELECT DISTINCT
+SELECT
     a.ANONID,
-    t."day of the year"
-FROM
-    FACTS f
-JOIN
-    TIMEDIM t ON f.TIMEID = t.ID
-JOIN
-    PORN_QUERIES p ON f.QUERYID = p.ID
-JOIN
-    ANONIDDIM a ON f.ANONID = a.ID
-WHERE
-    p.QUERY IS NOT NULL AND
-    f.CLICK IS NOT NULL
+    t."day of the year",
+    COUNT(*)
+FROM EXA_DB.FACTS f
+JOIN EXA_DB.TIMEDIM t ON f.TIMEID = t.ID
+JOIN EXA_DB.PORN_QUERIES p ON f.QUERYID = p.ID
+JOIN EXA_DB.ANONIDDIM a ON f.ANONID = a.ID
+WHERE p.QUERY IS NOT NULL AND f.CLICK IS NOT FALSE
+GROUP BY CUBE(a.ANONID, t."day of the year")
 ORDER BY
-    a.ANONID, t."day of the year";
+    CASE
+        WHEN GROUPING(a.ANONID) = 0 THEN a.ANONID
+        WHEN GROUPING(t."day of the year") = 0 THEN t."day of the year"
+    END;
